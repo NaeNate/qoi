a10c442616975924a2eb62dc95c3bb507a0777c0
Nate <nate@qstreet.org>
2024-11-08 23:58:43 -0500

    footer and use output vec instead of jsut wrigin

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,33 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let img = image::open(&args[1]).unwrap().to_rgba8();
 
-    let mut file = File::create("output.qoi").unwrap();
+    let width = img.width();
+    let height = img.height();
 
-    file.write(b"qoif").unwrap();
-    file.write(&img.width().to_be_bytes()).unwrap();
-    file.write(&img.height().to_be_bytes()).unwrap();
-    file.write(&(4 as u8).to_be_bytes()).unwrap();
-    file.write(&(0 as u8).to_be_bytes()).unwrap();
+    let mut output = Vec::new();
+
+    output.extend_from_slice(b"qoif");
+    output.extend(width.to_be_bytes());
+    output.extend(height.to_be_bytes());
+    output.push(4);
+    output.push(0);
 
     let mut seen = [(0, 0, 0, 255); 64];
     let mut previous = seen[0];
 
     for pixel in img.chunks(4) {
-        let current = (pixel[0], pixel[1], pixel[2], pixel[3]);
-        let hash = (pixel[0] * 3 + pixel[1] * 5 + pixel[2] * 7 + pixel[3] * 11) % 64;
+        if let &[r, g, b, a] = pixel {
+            let current = (r, g, b, a);
+            let hash = (r * 3 + g * 5 + b * 7 + a * 11) % 64;
 
-        seen[hash as usize] = current;
+            seen[hash as usize] = current;
 
-        previous = current;
+            previous = current;
+        }
     }
+
+    output.extend([0, 0, 0, 0, 0, 0, 0, 1]);
+
+    let mut file = File::create("output.qoi").unwrap();
+    file.write_all(&output).unwrap();
 }
